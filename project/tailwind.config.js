f16b2496270bb566c49721df30bcb1ecaa4d942b
prawneel <pranil.221734@ncit.edu.np>
2025-02-10 01:43:14 +0545

    portfolio

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
     extend: {
       animation: {
-        'spin-slow': 'spin 20s linear infinite',
+        'spin-slow': 'spin 8s linear infinite',
         'bounce-slow': 'bounce 3s infinite',
         'pulse-slow': 'pulse 3s infinite',
         'float': 'float 6s ease-in-out infinite',
@@ -13,6 +17,10 @@ export default {
         float: {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(-20px)' },
+        },
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
         }
       },
       boxShadow: {
